8bf20fe777ce6fe31c9c8d97860ec16bc8e0c6e9
Edots <erik.strahle94@gmail.com>
2022-12-21 10:35:28 +0100

    tack

--- app.js
+++ app.js
@@ -10,6 +10,53 @@ weatherAPI();
 // Startar bakgrundsbild på userpage.html
  startChangeBackground(changeBackground);
 
+	
+//  let submitArray = [{suggestion: , date: new Date()}];
+//  let index = 0;
+//  const area = document.getElementById("sgstInput");
+//  const getDate = new Date();
+
+//  function displayArray() {
+//     let element = document.getElementById("fromSuggestions");
+//     let getDate = document.getElementById("date");
+//      element.innerHTML = submitArray[index];
+//      index++;
+//      if (index === submitArray.length) {
+//          index = 0;
+//      }
+//  }
+ 
+//  function updateElement() {
+//      submitArray.push(getDate.toLocaleString() + " " + area.value);
+//      console.log(submitArray);
+//      localStorage.setItem("submitArray", JSON.stringify(submitArray));
+//  }
+
+ function retrieveData() {
+    // Get the stored data from local storage
+    const storedData = JSON.parse(localStorage.getItem("submitArray")) || [];
+  
+    // Update the submitArray variable with the stored data
+    submitArray = storedData;
+  }
+
+  document.getElementById("adminBtn").addEventListener("click", () => {
+    adminDashboard();
+    retrieveData();
+    displayArray();
+});
+
+ 
+ document.getElementById("showCase").addEventListener("click", displayArray);
+ // Function to push from förslagslåda
+ 
+ document.getElementById("sgstBtn").addEventListener("click", () => {
+        console.log("submitArray");
+        updateElement();
+ });
+
+
+
 
 // function getDateTime () {
 //     let date = new Date();
@@ -27,11 +74,11 @@ weatherAPI();
 
 
 
-document.getElementById("adminBtn").addEventListener("click", adminDashboard);
+
 
 
 // // Eventlistener för scrollfunktion för index.html
 document.getElementById("scrollUp").addEventListener("click", () => scroll('up'));
 document.getElementById("scrollDown").addEventListener("click", () => scroll('down'));
 
-//admin eventlistener 
+//admin eventlistener
